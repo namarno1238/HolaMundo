d5ca0fce5c24257502fdd3be3d931f94ffc9af88
Nacho <namarno08@gmail.com>
2025-11-18 19:12:20 +0100

    cambio

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,5 +1,5 @@
 public class HolaMundo {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hola, Mundo!");
+        System.out.println("Adios, Mundo!");
     }
 }
