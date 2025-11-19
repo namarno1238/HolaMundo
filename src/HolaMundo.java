7fdf12c210863d72d374155e183318a75c45cbc6
Nacho <namarno08@gmail.com>
2025-11-19 11:02:42 +0100

    Cambio Nachete

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,5 +1,5 @@
 public class HolaMundo {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hola, Mundillo!");
+        System.out.println("Hola, Nachete!");
     }
-}
+} 
